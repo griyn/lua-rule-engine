c12c06fb5ad0666e7914766757755d0e10f191ff
griyn <godlights@outlook.com>
2022-08-28 15:02:13 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include "lua_engine.h"
+#include "lua_rule_node.h"
 #include <iostream>
 
+
 int main() {
-	garden::LuaEngine lua_engine;
-	int r = lua_engine.init("function test()\n return 1 == 1\n end");
-	std::cout << r << std::endl;
-	r = lua_engine.is_function_exist("test");
-	std::cout << r << std::endl;
-	bool br = true;
-	r = lua_engine.call_lua_bool_function("test", &br);
-	std::cout << r << " " << br << std::endl;
+	garden::LuaRuleNode node;
+	garden::RuleConfig config;
+	config.set_name("test1");
+	config.set_expr("get_pb_int('n') == 1");
+	config.set_n(1);
+
+	if (node.init(config) != 0) {
+		std::cerr << "node init error" << std::endl;
+		return -1;
+	}
+
+	bool ret = node.run_rule(&config);
+	std::cout << "run_rule:" << ret << std::endl;
+
+	config.set_n(2);
+	ret = node.run_rule(&config);
+	std::cout << "run_rule:" << ret << std::endl;
+
 	return 0;
 }
